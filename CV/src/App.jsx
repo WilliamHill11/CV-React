845639bb5eced9d1bb651ec96250dc6b955ca9ea
william hill <williamhill0831@Hotmail.com>
2023-10-09 14:41:40 -0400

    setting up project

---
+++ CV/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+import './App.css';
+import PersonalForm from './components/PersonalForm/PersonalForm';
+
+function App() {
+  return (
+    <div>
+      <h1>My CV</h1>
+      <PersonalForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ CV/src/components/PersonalForm/PersonalForm.jsx
@@ -0,0 +1,9 @@
+function PersonalForm() {
+  return (
+    <div>
+      <h2>General Information</h2>
+    </div>
+  );
+}
+
+export default PersonalForm;
